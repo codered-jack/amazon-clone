a2acddd8cb3cd22f9e5d0bddd9cc770f815185d0
Sumit Kumar <sumit.kumar@pinelabs.com>
2020-11-17 13:54:24 +0530

    changes

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,37 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+
 const express = require("express");
+
 const cors = require("cors");
-const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
-);
+const { response } = require('express');
 
-// API
+const stripe = require("stripe")('sk_test_51HoLtYLoxlv18OKZjUTIjp9S2kzdX2x5a2DrTEEJALXkaUqlDWk339nS1ujcDrSijlMPz35BHmoLPMsaxyzSHuxW00R5hmLfaL')
 
-// - App config
+
 const app = express();
 
-// - Middlewares
-app.use(cors({ origin: true }));
+app.use(cors({origin:true}))
 app.use(express.json());
 
-// - API routes
-app.get("/", (request, response) => response.status(200).send("hello world"));
+app.get('/',(request,response)=>response.status(200).send('Hello World'))
 
-app.post("/payments/create", async (request, response) => {
-  const total = request.query.total;
+app.post('/payments/create',async (request,response)=>{
+    const total = request.query.total;
 
-  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+    console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // subunits of the currency
-    currency: "usd",
-  });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total, // subunits of the currency
+      currency: "usd",
+    });
 
-  // OK - Created
-  response.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+      });
 
-// - Listen command
-exports.api = functions.https.onRequest(app);
+})
 
-// Example endpoint
-// http://localhost:5001/challenge-4b2b2/us-central1/api
+exports.api = functions.https.onRequest(app) 
+
+
+//http://localhost:5001/clone-fb9f7/us-central1/api
--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 
 const promise = loadStripe(
-  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+  "pk_test_51HoLtYLoxlv18OKZ9Wd6eYcJxn0fNxuOsaedtCbkugqb2QhMqrYpGYVaMDTdb47lHMQAStx2dBtooPaGaQ7XOgOe00aHYOKlTv"
 );
 
 function App() {
